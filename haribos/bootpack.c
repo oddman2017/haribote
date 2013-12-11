ada60c5199a17545fec8ceba68226a863ae6ab7c
Ralph Shane <free2000fly@gmail.com>
2013-12-11 16:47:12 +0800

    Day 04, harib01d

--- haribos/bootpack.c
+++ haribos/bootpack.c
@@ -2,15 +2,13 @@ void io_hlt(void);
 
 void HariMain(void)
 {
-	int i = 0xa0000; /* 変数宣言。iという変数は、32ビットの整数型 */
+	int i; /* 変数宣言。iという変数は、32ビットの整数型 */
 	char *p; /* pという変数は、BYTE [...]用の番地 */
 
-	for (i = 0xa0000; i <= 0xaffff; i++) {
+	p = (char *) 0xa0000; /* 番地を代入 */
 
-		p = (char *) i; /* 番地を代入 */
-		*p = i & 0x0f;
-
-		/* これで write_mem8(i, i & 0x0f); の代わりになる */
+	for (i = 0; i <= 0xffff; i++) {
+		*(p + i) = i & 0x0f;
 	}
 
 	for (;;) {
