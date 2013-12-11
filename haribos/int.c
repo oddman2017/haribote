1078b9f7bd881be92df1673e7a28f0bc95a9ff49
Ralph Shane <free2000fly@gmail.com>
2013-12-11 17:52:39 +0800

    Day 07, harib04c

--- haribos/int.c
+++ haribos/int.c
@@ -34,9 +34,9 @@ void inthandler21(int *esp)
 	unsigned char data;
 	io_out8(PIC0_OCW2, 0x61);	/* IRQ-01受付完了をPICに通知 */
 	data = io_in8(PORT_KEYDAT);
-	if (keybuf.flag == 0) {
-		keybuf.data = data;
-		keybuf.flag = 1;
+	if (keybuf.next < 32) {
+		keybuf.data[keybuf.next] = data;
+		keybuf.next++;
 	}
 	return;
 }
