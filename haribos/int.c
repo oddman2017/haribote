3b0204183079cd4ab807997a3442a741e1177153
Ralph Shane <free2000fly@gmail.com>
2013-12-11 17:41:12 +0800

    Day 06, harib03e

--- haribos/int.c
+++ haribos/int.c
@@ -23,3 +23,37 @@ void init_pic(void)
 
 	return;
 }
+
+void inthandler21(int *esp)
+/* PS/2キーボードからの割り込み */
+{
+	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
+	boxfill8(binfo->vram, binfo->scrnx, COL8_000000, 0, 0, 32 * 8 - 1, 15);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, "INT 21 (IRQ-1) : PS/2 keyboard");
+	for (;;) {
+		io_hlt();
+	}
+}
+
+void inthandler2c(int *esp)
+/* PS/2マウスからの割り込み */
+{
+	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
+	boxfill8(binfo->vram, binfo->scrnx, COL8_000000, 0, 0, 32 * 8 - 1, 15);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, "INT 2C (IRQ-12) : PS/2 mouse");
+	for (;;) {
+		io_hlt();
+	}
+}
+
+void inthandler27(int *esp)
+/* PIC0からの不完全割り込み対策 */
+/* Athlon64X2機などではチップセットの都合によりPICの初期化時にこの割り込みが1度だけおこる */
+/* この割り込み処理関数は、その割り込みに対して何もしないでやり過ごす */
+/* なぜ何もしなくていいの？
+	→  この割り込みはPIC初期化時の電気的なノイズによって発生したものなので、
+		まじめに何か処理してやる必要がない。									*/
+{
+	io_out8(PIC0_OCW2, 0x67); /* IRQ-07受付完了をPICに通知(7-1参照) */
+	return;
+}
