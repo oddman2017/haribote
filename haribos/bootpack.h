2d5f60cbc0df04cae047058689341c22f76197a8
Ralph Shane <free2000fly@gmail.com>
2013-12-11 18:37:04 +0800

    Day 10, harib07

--- haribos/bootpack.h
+++ haribos/bootpack.h
@@ -9,8 +9,6 @@ struct BOOTINFO { /* 0x0ff0-0x0fff */
 };
 #define ADR_BOOTINFO	0x00000ff0
 
-#define _countof(_Array) (sizeof(_Array) / sizeof(_Array[0]))
-
 /* naskfunc.nas */
 void io_hlt(void);
 void io_cli(void);
@@ -123,3 +121,21 @@ void inthandler2c(int *esp);
 void enable_mouse(struct MOUSE_DEC *mdec);
 int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
 extern struct FIFO8 mousefifo;
+
+/* memory.c */
+#define MEMMAN_FREES		4090	/* これで約32KB */
+#define MEMMAN_ADDR			0x003c0000
+struct FREEINFO {	/* あき情報 */
+	unsigned int addr, size;
+};
+struct MEMMAN {		/* メモリ管理 */
+	int frees, maxfrees, lostsize, losts;
+	struct FREEINFO free[MEMMAN_FREES];
+};
+unsigned int memtest(unsigned int start, unsigned int end);
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
+unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size);
+int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size);
