4f0a16fa431932b0036bfa8b20e42eb7e3d0b839
Ralph Shane <free2000fly@gmail.com>
2013-12-11 18:42:27 +0800

    Day 10, harib07d

--- haribos/sheet.c
+++ haribos/sheet.c
@@ -49,21 +49,28 @@ void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, i
 
 void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1)
 {
-	int h, bx, by, vx, vy;
+	int h, bx, by, vx, vy, bx0, by0, bx1, by1;
 	unsigned char *buf, c, *vram = ctl->vram;
 	struct SHEET *sht;
 	for (h = 0; h <= ctl->top; h++) {
 		sht = ctl->sheets[h];
 		buf = sht->buf;
-		for (by = 0; by < sht->bysize; by++) {
+		/* vx0～vy1を使って、bx0～by1を逆算する */
+		bx0 = vx0 - sht->vx0;
+		by0 = vy0 - sht->vy0;
+		bx1 = vx1 - sht->vx0;
+		by1 = vy1 - sht->vy0;
+		if (bx0 < 0) { bx0 = 0; }
+		if (by0 < 0) { by0 = 0; }
+		if (bx1 > sht->bxsize) { bx1 = sht->bxsize; }
+		if (by1 > sht->bysize) { by1 = sht->bysize; }
+		for (by = by0; by < by1; by++) {
 			vy = sht->vy0 + by;
-			for (bx = 0; bx < sht->bxsize; bx++) {
+			for (bx = bx0; bx < bx1; bx++) {
 				vx = sht->vx0 + bx;
-				if (vx0 <= vx && vx < vx1 && vy0 <= vy && vy < vy1) {
-					c = buf[by * sht->bxsize + bx];
-					if (c != sht->col_inv) {
-						vram[vy * ctl->xsize + vx] = c;
-					}
+				c = buf[by * sht->bxsize + bx];
+				if (c != sht->col_inv) {
+					vram[vy * ctl->xsize + vx] = c;
 				}
 			}
 		}
