cba081fccf30c62859ddb1da2a62c16365c36e86
Ralph Shane <free2000fly@gmail.com>
2013-12-12 11:17:28 +0800

    Day 17, harib14b

--- haribos/bootpack.h
+++ haribos/bootpack.h
@@ -213,6 +213,7 @@ struct TASKCTL {
 	struct TASK tasks0[MAX_TASKS];
 };
 extern struct TIMER *task_timer;
+struct TASK *task_now(void);
 struct TASK *task_init(struct MEMMAN *memman);
 struct TASK *task_alloc(void);
 void task_run(struct TASK *task, int level, int priority);
