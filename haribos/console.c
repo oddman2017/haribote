c73296bbb44536d4abffc5ebd5913943886a1d97
Ralph Shane <free2000fly@gmail.com>
2013-12-12 13:32:37 +0800

    Day 23, harib20b

--- haribos/console.c
+++ haribos/console.c
@@ -356,6 +356,10 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 	} else if (edx == 10) {
 		ecx = (ecx + 0x0f) & 0xfffffff0; /* 16バイト単位に切り上げ */
 		memman_free((struct MEMMAN *) (ebx + ds_base), eax, ecx);
+	} else if (edx == 11) {
+		sht = (struct SHEET *) ebx;
+		sht->buf[sht->bxsize * edi + esi] = eax;
+		sheet_refresh(sht, esi, edi, esi + 1, edi + 1);
 	}
 	return 0;
 }
