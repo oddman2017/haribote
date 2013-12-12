b60edf031d9585f895555f2cee62b390331cf6c0
Ralph Shane <free2000fly@gmail.com>
2013-12-12 11:04:23 +0800

    Day 16, harib13b

--- haribos/mtask.c
+++ haribos/mtask.c
@@ -75,3 +75,39 @@ void task_switch(void)
 	}
 	return;
 }
+
+void task_sleep(struct TASK *task)
+{
+	int i;
+	char ts = 0;
+	if (task->flags == 2) {		/* 指定タスクがもし起きていたら */
+		if (task == taskctl->tasks[taskctl->now]) {
+			ts = 1; /* 自分自身を寝かせるので、あとでタスクスイッチする */
+		}
+		/* taskがどこにいるかを探す */
+		for (i = 0; i < taskctl->running; i++) {
+			if (taskctl->tasks[i] == task) {
+				/* ここにいた */
+				break;
+			}
+		}
+		taskctl->running--;
+		if (i < taskctl->now) {
+			taskctl->now--; /* ずれるので、これもあわせておく */
+		}
+		/* ずらし */
+		for (; i < taskctl->running; i++) {
+			taskctl->tasks[i] = taskctl->tasks[i + 1];
+		}
+		task->flags = 1; /* 動作していない状態 */
+		if (ts != 0) {
+			/* タスクスイッチする */
+			if (taskctl->now >= taskctl->running) {
+				/* nowがおかしな値になっていたら、修正する */
+				taskctl->now = 0;
+			}
+			farjmp(0, taskctl->tasks[taskctl->now]->sel);
+		}
+	}
+	return;
+}
