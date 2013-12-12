039ff34a2d656d2402d042e2567b18536d238869
Ralph Shane <free2000fly@gmail.com>
2013-12-12 13:20:28 +0800

    Day 22, harib19d

--- haribos/a_nask.nas
+++ haribos/a_nask.nas
@@ -4,6 +4,7 @@
 [FILE "a_nask.nas"]
 
 		GLOBAL	_api_putchar
+		GLOBAL	_api_putstr0
 		GLOBAL	_api_end
 
 [SECTION .text]
@@ -12,6 +13,14 @@ _api_putchar:	; void api_putchar(int c);
 		MOV		EDX,1
 		MOV		AL,[ESP+4]		; c
 		INT		0x40
+		RET
+
+_api_putstr0:	; void api_putstr0(char *s);
+		PUSH	EBX
+		MOV		EDX,2
+		MOV		EBX,[ESP+8]		; s
+		INT		0x40
+		POP		EBX
 		RET
 
 _api_end:	; void api_end(void);
--- haribos/console.c
+++ haribos/console.c
@@ -291,14 +291,10 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
 		set_segmdesc(gdt + 1003, finfo->size - 1, (int) p, AR_CODE32_ER + 0x60);
 		set_segmdesc(gdt + 1004, 64 * 1024 - 1,   (int) q, AR_DATA32_RW + 0x60);
 		if (finfo->size >= 8 && strncmp(p + 4, "Hari", 4) == 0) {
-			p[0] = 0xe8;
-			p[1] = 0x16;
-			p[2] = 0x00;
-			p[3] = 0x00;
-			p[4] = 0x00;
-			p[5] = 0xcb;
+			start_app(0x1b, 1003 * 8, 64 * 1024, 1004 * 8, &(task->tss.esp0));
+		} else {
+			start_app(0, 1003 * 8, 64 * 1024, 1004 * 8, &(task->tss.esp0));
 		}
-		start_app(0, 1003 * 8, 64 * 1024, 1004 * 8, &(task->tss.esp0));
 		memman_free_4k(memman, (int) p, finfo->size);
 		memman_free_4k(memman, (int) q, 64 * 1024);
 		cons_newline(cons);
