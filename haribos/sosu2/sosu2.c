dd07f7763670de44c98e7dda8eb6e0ece9b079ad
Ralph Shane <free2000fly@gmail.com>
2013-12-12 18:16:04 +0800

    Day 28, harib25a

---
+++ haribos/sosu2/sosu2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "apilib.h"
+
+#define MAX		10000
+
+void HariMain(void)
+{
+	char flag[MAX], s[8];
+	int i, j;
+	for (i = 0; i < MAX; i++) {
+		flag[i] = 0;
+	}
+	for (i = 2; i < MAX; i++) {
+		if (flag[i] == 0) {
+			/* 印がついていないので素数だ！ */
+			sprintf(s, "%d ", i);
+			api_putstr0(s);
+			for (j = i * 2; j < MAX; j += i) {
+				flag[j] = 1;	/* 倍数には印をつける */
+			}
+		}
+	}
+	api_end();
+}
