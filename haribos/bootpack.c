48dfc01856600a660cb2af0309502f173318bd9f
Ralph Shane <free2000fly@gmail.com>
2013-12-12 16:45:32 +0800

    Day 25, harib22h

--- haribos/bootpack.c
+++ haribos/bootpack.c
@@ -21,7 +21,7 @@ void HariMain(void)
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 	unsigned char *buf_back, buf_mouse[256], *buf_win, *buf_cons[2];
 	struct SHEET *sht_back, *sht_mouse, *sht_win, *sht_cons[2];
-	struct TASK *task_a, *task_cons[2];
+	struct TASK *task_a, *task_cons[2], *task;
 	struct TIMER *timer;
 	static char keytable0[0x80] = {
 		0,   0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', 0,   0,
@@ -44,7 +44,6 @@ void HariMain(void)
 		0,   0,   0,   '_', 0,   0,   0,   0,   0,   0,   0,   0,   0,   '|', 0,   0
 	};
 	int key_shift = 0, key_leds = (binfo->leds >> 4) & 7, keycmd_wait = -1;
-	struct CONSOLE *cons;
 	int j, x, y, mmx = -1, mmy = -1;
 	struct SHEET *sht = 0, *key_win;
 
@@ -233,13 +232,15 @@ void HariMain(void)
 					fifo32_put(&keycmd, KEYCMD_LED);
 					fifo32_put(&keycmd, key_leds);
 				}
-				if (i == 256 + 0x3b && key_shift != 0 && task_cons[0]->tss.ss0 != 0) {	/* Shift+F1 */
-					cons = (struct CONSOLE *) *((int *) 0x0fec);
-					cons_putstr0(cons, "\nBreak(key) :\n");
-					io_cli();	/* 強制終了処理中にタスクが変わると困るから */
-					task_cons[0]->tss.eax = (int) &(task_cons[0]->tss.esp0);
-					task_cons[0]->tss.eip = (int) asm_end_app;
-					io_sti();
+				if (i == 256 + 0x3b && key_shift != 0) {
+					task = key_win->task;
+					if (task != 0 && task->tss.ss0 != 0) {	/* Shift+F1 */
+						cons_putstr0(task->cons, "\nBreak(key) :\n");
+						io_cli();	/* 強制終了処理中にタスクが変わると困るから */
+						task->tss.eax = (int) &(task->tss.esp0);
+						task->tss.eip = (int) asm_end_app;
+						io_sti();
+					}
 				}
 				if (i == 256 + 0x57) {	/* F11 */
 					sheet_updown(shtctl->sheets[1], shtctl->top - 1);
@@ -298,11 +299,11 @@ void HariMain(void)
 										if (sht->bxsize - 21 <= x && x < sht->bxsize - 5 && 5 <= y && y < 19) {
 											/* 「×」ボタンクリック */
 											if ((sht->flags & 0x10) != 0) {		/* アプリが作ったウィンドウか？ */
-												cons = (struct CONSOLE *) *((int *) 0x0fec);
-												cons_putstr0(cons, "\nBreak(mouse) :\n");
+												task = sht->task;
+												cons_putstr0(task->cons, "\nBreak(mouse) :\n");
 												io_cli();	/* 強制終了処理中にタスクが変わると困るから */
-												task_cons[0]->tss.eax = (int) &(task_cons[0]->tss.esp0);
-												task_cons[0]->tss.eip = (int) asm_end_app;
+												task->tss.eax = (int) &(task->tss.esp0);
+												task->tss.eip = (int) asm_end_app;
 												io_sti();
 											}
 										}
