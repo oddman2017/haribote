ef43e934d9eff7ebf6e41b03c1385b776d459eb0
Ralph Shane <free2000fly@gmail.com>
2013-12-12 17:18:03 +0800

    Day 27, harib24b

--- haribos/bootpack.c
+++ haribos/bootpack.c
@@ -46,7 +46,7 @@ void HariMain(void)
 	};
 	int key_shift = 0, key_leds = (binfo->leds >> 4) & 7, keycmd_wait = -1;
 	int j, x, y, mmx = -1, mmy = -1, mmx2 = 0;
-	struct SHEET *sht = 0, *key_win;
+	struct SHEET *sht = 0, *key_win, *sht2;
 
 	init_gdtidt();
 	init_pic();
@@ -273,6 +273,10 @@ void HariMain(void)
 												task_run(task, -1, 0);
 											} else {	/* コンソール */
 												task = sht->task;
+												sheet_updown(sht, -1); /* とりあえず非表示にしておく */
+												keywin_off(key_win);
+												key_win = shtctl->sheets[shtctl->top - 1];
+												keywin_on(key_win);
 												io_cli();
 												fifo32_put(&task->fifo, 4);
 												io_sti();
@@ -303,6 +307,10 @@ void HariMain(void)
 				close_console(shtctl->sheets0 + (i - 768));
 			} else if (1024 <= i && i <= 2023) {
 				close_constask(taskctl->tasks0 + (i - 1024));
+			} else if (2024 <= i && i <= 2279) {	/* コンソールだけを閉じる */
+				sht2 = shtctl->sheets0 + (i - 2024);
+				memman_free_4k(memman, (int) sht2->buf, 256 * 165);
+				sheet_free(sht2);
 			}
 		}
 	}
--- haribos/console.c
+++ haribos/console.c
@@ -17,7 +17,7 @@ void console_task(struct SHEET *sheet, int memtotal)
 	cons.cur_c = -1;
 	task->cons = &cons;
 
-	if (sheet != 0) {
+	if (cons.sht != 0) {
 		cons.timer = timer_alloc();
 		timer_init(cons.timer, &task->fifo, 1);
 		timer_settime(cons.timer, 50);
@@ -35,7 +35,7 @@ void console_task(struct SHEET *sheet, int memtotal)
 		} else {
 			i = fifo32_get(&task->fifo);
 			io_sti();
-			if (i <= 1) { /* カーソル用タイマ */
+			if (i <= 1 && cons.sht != 0) { /* カーソル用タイマ */
 				if (i != 0) {
 					timer_init(cons.timer, &task->fifo, 0); /* 次は0を */
 					if (cons.cur_c >= 0) {
@@ -53,8 +53,8 @@ void console_task(struct SHEET *sheet, int memtotal)
 				cons.cur_c = COL8_FFFFFF;
 			}
 			if (i == 3) {	/* カーソルOFF */
-				if (sheet != 0) {
-					boxfill8(sheet->buf, sheet->bxsize, COL8_000000,
+				if (cons.sht != 0) {
+					boxfill8(cons.sht->buf, cons.sht->bxsize, COL8_000000,
 						cons.cur_x, cons.cur_y, cons.cur_x + 7, cons.cur_y + 15);
 				}
 				cons.cur_c = -1;
@@ -77,7 +77,7 @@ void console_task(struct SHEET *sheet, int memtotal)
 					cmdline[cons.cur_x / 8 - 2] = 0;
 					cons_newline(&cons);
 					cons_runcmd(cmdline, &cons, fat, memtotal);	/* コマンド実行 */
-					if (sheet == 0) {
+					if (cons.sht == 0) {
 						cmd_exit(&cons, fat);
 					}
 					/* プロンプト表示 */
@@ -92,12 +92,12 @@ void console_task(struct SHEET *sheet, int memtotal)
 				}
 			}
 			/* カーソル再表示 */
-			if (sheet != 0) {
+			if (cons.sht != 0) {
 				if (cons.cur_c >= 0) {
-					boxfill8(sheet->buf, sheet->bxsize, cons.cur_c, 
+					boxfill8(cons.sht->buf, cons.sht->bxsize, cons.cur_c, 
 						cons.cur_x, cons.cur_y, cons.cur_x + 7, cons.cur_y + 15);
 				}
-				sheet_refresh(sheet, cons.cur_x, cons.cur_y, cons.cur_x + 8, cons.cur_y + 16);
+				sheet_refresh(cons.sht, cons.cur_x, cons.cur_y, cons.cur_x + 8, cons.cur_y + 16);
 			}
 		}
 	}
@@ -406,6 +406,7 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 	struct CONSOLE *cons = task->cons;
 	struct SHTCTL *shtctl = (struct SHTCTL *) *((int *) 0x0fe4);
 	struct SHEET *sht;
+	struct FIFO32 *sys_fifo = (struct FIFO32 *) *((int *) 0x0fec);
 	int *reg = &eax + 1;	/* eaxの次の番地 */
 		/* 保存のためのPUSHADを強引に書き換える */
 		/* reg[0] : EDI,   reg[1] : ESI,   reg[2] : EBP,   reg[3] : ESP */
@@ -482,7 +483,7 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 			}
 			i = fifo32_get(&task->fifo);
 			io_sti();
-			if (i <= 1) { /* カーソル用タイマ */
+			if (i <= 1 && cons->sht != 0) { /* カーソル用タイマ */
 				/* アプリ実行中はカーソルが出ないので、いつも次は表示用の1を注文しておく */
 				timer_init(cons->timer, &task->fifo, 1); /* 次は1を */
 				timer_settime(cons->timer, 50);
@@ -492,6 +493,13 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 			}
 			if (i == 3) {	/* カーソルOFF */
 				cons->cur_c = -1;
+			}
+			if (i == 4) {	/* コンソールだけを閉じる */
+				timer_cancel(cons->timer);
+				io_cli();
+				fifo32_put(sys_fifo, cons->sht - shtctl->sheets0 + 2024);	/* 2024～2279 */
+				cons->sht = 0;
+				io_sti();
 			}
 			if (i >= 256) { /* キーボードデータ（タスクA経由）など */
 				reg[7] = i - 256;
