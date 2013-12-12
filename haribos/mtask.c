9e7fb7830fbb1eafd5ddf7162362fb8db5432d69
Ralph Shane <free2000fly@gmail.com>
2013-12-12 11:11:15 +0800

    Day 16, harib13d

--- haribos/mtask.c
+++ haribos/mtask.c
@@ -18,12 +18,13 @@ struct TASK *task_init(struct MEMMAN *memman)
 	}
 	task = task_alloc();
 	task->flags = 2; /* 動作中マーク */
+	task->priority = 2; /* 0.02秒 */
 	taskctl->running = 1;
 	taskctl->now = 0;
 	taskctl->tasks[0] = task;
 	load_tr(task->sel);
 	task_timer = timer_alloc();
-	timer_settime(task_timer, 2);
+	timer_settime(task_timer, task->priority);
 	return task;
 }
 
@@ -55,23 +56,30 @@ struct TASK *task_alloc(void)
 	return 0; /* もう全部使用中 */
 }
 
-void task_run(struct TASK *task)
+void task_run(struct TASK *task, int priority)
 {
-	task->flags = 2; /* 動作中マーク */
-	taskctl->tasks[taskctl->running] = task;
-	taskctl->running++;
+	if (priority > 0) {
+		task->priority = priority;
+	}
+	if (task->flags != 2) {
+		task->flags = 2; /* 動作中マーク */
+		taskctl->tasks[taskctl->running] = task;
+		taskctl->running++;
+	}
 	return;
 }
 
 void task_switch(void)
 {
-	timer_settime(task_timer, 2);
+	struct TASK *task;
+	taskctl->now++;
+	if (taskctl->now == taskctl->running) {
+		taskctl->now = 0;
+	}
+	task = taskctl->tasks[taskctl->now];
+	timer_settime(task_timer, task->priority);
 	if (taskctl->running >= 2) {
-		taskctl->now++;
-		if (taskctl->now == taskctl->running) {
-			taskctl->now = 0;
-		}
-		farjmp(0, taskctl->tasks[taskctl->now]->sel);
+		farjmp(0, task->sel);
 	}
 	return;
 }
