a944d3bee5edd6340263ff6c8f7b44936663c1a9
Ralph Shane <free2000fly@gmail.com>
2013-12-12 10:11:04 +0800

    Day 12, harib09b

--- haribos/timer.c
+++ haribos/timer.c
@@ -5,17 +5,20 @@
 #define PIT_CTRL	0x0043
 #define PIT_CNT0	0x0040
 
+struct TIMERCTL timerctl;
+
 void init_pit(void)
 {
 	io_out8(PIT_CTRL, 0x34);
 	io_out8(PIT_CNT0, 0x9c);
 	io_out8(PIT_CNT0, 0x2e);
+	timerctl.count = 0;
 	return;
 }
 
 void inthandler20(int *esp)
 {
 	io_out8(PIC0_OCW2, 0x60);	/* IRQ-00受付完了をPICに通知 */
-	/* とりあえずなにもしない */
+	timerctl.count++;
 	return;
 }
