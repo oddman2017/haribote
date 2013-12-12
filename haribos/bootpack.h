6b34c9a4f2627a1c010acf6ba98685edead8a73b
Ralph Shane <free2000fly@gmail.com>
2013-12-12 11:01:50 +0800

    Day 16, harib13a

--- haribos/bootpack.h
+++ haribos/bootpack.h
@@ -185,6 +185,26 @@ void timer_settime(struct TIMER *timer, unsigned int timeout);
 void inthandler20(int *esp);
 
 /* mtask.c */
-extern struct TIMER *mt_timer;
-void mt_init(void);
-void mt_taskswitch(void);
+#define MAX_TASKS		1000	/* 最大タスク数 */
+#define TASK_GDT0		3		/* TSSをGDTの何番から割り当てるのか */
+struct TSS32 {
+	int backlink, esp0, ss0, esp1, ss1, esp2, ss2, cr3;
+	int eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi;
+	int es, cs, ss, ds, fs, gs;
+	int ldtr, iomap;
+};
+struct TASK {
+	int sel, flags; /* selはGDTの番号のこと */
+	struct TSS32 tss;
+};
+struct TASKCTL {
+	int running; /* 動作しているタスクの数 */
+	int now; /* 現在動作しているタスクがどれだか分かるようにするための変数 */
+	struct TASK *tasks[MAX_TASKS];
+	struct TASK tasks0[MAX_TASKS];
+};
+extern struct TIMER *task_timer;
+struct TASK *task_init(struct MEMMAN *memman);
+struct TASK *task_alloc(void);
+void task_run(struct TASK *task);
+void task_switch(void);
