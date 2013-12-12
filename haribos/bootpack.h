288cf3c38c89bd7b0e53e9e2960b6cabffc68348
Ralph Shane <free2000fly@gmail.com>
2013-12-12 17:22:37 +0800

    Day 27, harib24d

--- haribos/bootpack.h
+++ haribos/bootpack.h
@@ -96,6 +96,7 @@ void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
 #define LIMIT_BOTPAK	0x0007ffff
 #define AR_DATA32_RW	0x4092
 #define AR_CODE32_ER	0x409a
+#define AR_LDT			0x0082
 #define AR_TSS32		0x0089
 #define AR_INTGATE32	0x008e
 
@@ -210,6 +211,7 @@ struct TASK {
 	int level, priority;
 	struct FIFO32 fifo;
 	struct TSS32 tss;
+	struct SEGMENT_DESCRIPTOR ldt[2];
 	struct CONSOLE *cons;
 	int ds_base, cons_stack;
 };
