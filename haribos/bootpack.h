3ced08acb6c121db4903abcf6c14101d3091e50f
Ralph Shane <free2000fly@gmail.com>
2013-12-12 10:06:11 +0800

    Day 11, harib08h

--- haribos/bootpack.h
+++ haribos/bootpack.h
@@ -148,7 +148,7 @@ struct SHEET {
 	struct SHTCTL *ctl;
 };
 struct SHTCTL {
-	unsigned char *vram;
+	unsigned char *vram, *map;
 	int xsize, ysize, top;
 	struct SHEET *sheets[MAX_SHEETS];
 	struct SHEET sheets0[MAX_SHEETS];
