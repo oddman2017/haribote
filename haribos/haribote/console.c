1c5b3524993213c22b0aa5696c70c1b570ac57f2
Ralph Shane <free2000fly@gmail.com>
2013-12-12 18:56:22 +0800

    Day 28, harib25f

--- haribos/haribote/console.c
+++ haribos/haribote/console.c
@@ -37,6 +37,7 @@ void console_task(struct SHEET *sheet, int memtotal)
 	} else {
 		task->langmode = 0;
 	}
+	task->langbyte1 = 0;
 
 	/* プロンプト表示 */
 	cons_putchar(&cons, '>', 1);
@@ -158,6 +159,7 @@ void cons_newline(struct CONSOLE *cons)
 {
 	int x, y;
 	struct SHEET *sheet = cons->sht;
+	struct TASK *task = task_now();
 	if (cons->cur_y < 28 + 112) {
 		cons->cur_y += 16; /* 次の行へ */
 	} else {
@@ -177,6 +179,9 @@ void cons_newline(struct CONSOLE *cons)
 		}
 	}
 	cons->cur_x = 8;
+	if (task->langmode == 1 && task->langbyte1 != 0) {
+		cons->cur_x = 16;
+	}
 	return;
 }
 
@@ -401,6 +406,7 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
 			}
 			timer_cancelall(&task->fifo);
 			memman_free_4k(memman, (int) q, segsiz);
+			task->langbyte1 = 0;
 		} else {
 			cons_putstr0(cons, ".hrb file format error.\n");
 		}
