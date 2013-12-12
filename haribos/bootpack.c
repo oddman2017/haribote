9061f5f91e232fb6775ffa33ba2e04e3697e3f81
Ralph Shane <free2000fly@gmail.com>
2013-12-12 16:49:17 +0800

    Day 25, harib22j

--- haribos/bootpack.c
+++ haribos/bootpack.c
@@ -14,7 +14,7 @@ void HariMain(void)
 	struct SHTCTL *shtctl;
 	char s[40];
 	struct FIFO32 fifo, keycmd;
-	int fifobuf[128], keycmd_buf[32];
+	int fifobuf[128], keycmd_buf[32], *cons_fifo[2];
 	int mx, my, i;
 	unsigned int memtotal;
 	struct MOUSE_DEC mdec;
@@ -96,6 +96,8 @@ void HariMain(void)
 		task_run(task_cons[i], 2, 2); /* level=2, priority=2 */
 		sht_cons[i]->task = task_cons[i];
 		sht_cons[i]->flags |= 0x20;	/* カーソルあり */
+		cons_fifo[i] = (int *) memman_alloc_4k(memman, 128 * 4);
+		fifo32_init(&task_cons[i]->fifo, 128, cons_fifo[i], task_cons[i]);
 	}
 
 	/* sht_mouse */
--- haribos/console.c
+++ haribos/console.c
@@ -8,7 +8,7 @@ void console_task(struct SHEET *sheet, int memtotal)
 {
 	struct TASK *task = task_now();
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
-	int i, fifobuf[128], *fat = (int *) memman_alloc_4k(memman, 4 * 2880);
+	int i, *fat = (int *) memman_alloc_4k(memman, 4 * 2880);
 	struct CONSOLE cons;
 	char cmdline[30];
 	cons.sht = sheet;
@@ -17,7 +17,6 @@ void console_task(struct SHEET *sheet, int memtotal)
 	cons.cur_c = -1;
 	task->cons = &cons;
 
-	fifo32_init(&task->fifo, 128, fifobuf, task);
 	cons.timer = timer_alloc();
 	timer_init(cons.timer, &task->fifo, 1);
 	timer_settime(cons.timer, 50);
